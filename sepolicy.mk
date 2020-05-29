f4071151681b25db711114b850ddaff624daac68
ThatisJigen <gianfranco.liguori.96@gmail.com>
2020-05-29 15:38:27 +0200

    Sepolicy: sdm710 custom location

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,60 +1,60 @@
 # Board specific SELinux policy variable definitions
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public
+    device/qcom/sepolicy-sdm710/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private
+    device/qcom/sepolicy-sdm710/generic/private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/qva/public
+    device/qcom/sepolicy-sdm710/qva/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/qva/private
+    device/qcom/sepolicy-sdm710/qva/private
 
 # sepolicy rules for product images
 PRODUCT_PUBLIC_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/product/public
+    device/qcom/sepolicy-sdm710/product/public
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/product/private
+    device/qcom/sepolicy-sdm710/product/private
 
 ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS += \
-        device/qcom/sepolicy/generic/vendor/common \
-        device/qcom/sepolicy/qva/vendor/common/sysmonapp \
-        device/qcom/sepolicy/qva/vendor/ssg \
-        device/qcom/sepolicy/timeservice \
-        device/qcom/sepolicy/qva/vendor/qwesas \
-        device/qcom/sepolicy/qva/vendor/common
+        device/qcom/sepolicy-sdm710/generic/vendor/common \
+        device/qcom/sepolicy-sdm710/qva/vendor/common/sysmonapp \
+        device/qcom/sepolicy-sdm710/qva/vendor/ssg \
+        device/qcom/sepolicy-sdm710/timeservice \
+        device/qcom/sepolicy-sdm710/qva/vendor/qwesas \
+        device/qcom/sepolicy-sdm710/qva/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/generic/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/qva/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/generic/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/qva/vendor/$(TARGET_SEPOLICY_DIR)
     endif
 
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/test
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/test
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/generic/vendor/test
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/qva/vendor/test
     endif
 endif
 
 ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS += \
-        device/qcom/sepolicy/legacy/vendor/common/sysmonapp \
-        device/qcom/sepolicy/legacy/vendor/ssg \
-        device/qcom/sepolicy/timeservice \
-        device/qcom/sepolicy/legacy/vendor/common
+        device/qcom/sepolicy-sdm710/legacy/vendor/common/sysmonapp \
+        device/qcom/sepolicy-sdm710/legacy/vendor/ssg \
+        device/qcom/sepolicy-sdm710/timeservice \
+        device/qcom/sepolicy-sdm710/legacy/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/legacy/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/legacy/vendor/$(TARGET_SEPOLICY_DIR)
     endif
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/test
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-sdm710/legacy/vendor/test
     endif
 endif
 
